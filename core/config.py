babd1dfb74c1d3fd6c041ba655bf21170e0cbce3
Tayyab athar <tayyab.dev.acc@gmail.com>
2023-10-01 06:08:28 +0500

    Database connection fixes

--- core/config.py
+++ core/config.py
@@ -1,6 +1,10 @@
 from typing import Any, Dict, Optional
 
-from pydantic import BaseSettings, PostgresDsn, validator
+from dotenv import load_dotenv
+from pydantic import validator
+from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
@@ -9,20 +13,20 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
     POSTGRES_PORT: str
-    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+    SQLALCHEMY_DATABASE_URI: Optional[str] = None
 
     @validator("SQLALCHEMY_DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
             return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_HOST"),
-            port=values.get("POSTGRES_PORT"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
-        )
+
+        user = values.get("POSTGRES_USER")
+        password = values.get("POSTGRES_PASSWORD")
+        host = values.get("POSTGRES_HOST")
+        database = values.get("POSTGRES_DB")
+        port = values.get("POSTGRES_PORT")
+
+        return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
 
     class Config:
         case_sensitive = True
